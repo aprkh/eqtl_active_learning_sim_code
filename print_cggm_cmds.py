25b2f990599bf293b904c6bc0513131075be8837
aprkh <68245961+aprkh@users.noreply.github.com>
2022-05-11 19:21:19 -0400

    Updated print_cggm_cmds.py

--- print_cggm_cmds.py
+++ print_cggm_cmds.py
@@ -3,11 +3,14 @@
 # prints commands for cggm output from simulations.
 ######################################################################
 
+# for timing 
+import time
+
 import sys
 import subprocess
 
 GENERAL_PREFIX = "/mnt/c/Users/apare/Desktop/KimResearchGroup/Spring2022/"
-TO_CITRUSS = GENERAL_PREFIX + "mlcggm/Mega-sCGGM_python/citruss.py"
+TO_CITRUSS = GENERAL_PREFIX + "mlcggm/Mega-sCGGM_cpp/citruss"
 TO_DATA = GENERAL_PREFIX + "input_simulation/simulateCode2/missing"
 
 N = 2000
@@ -34,18 +37,20 @@ def main():
             xm_fname = TO_DATA + "{}/Xm{}.txt".format(ratio, i)
             xp_fname = TO_DATA + "{}/Xp{}.txt".format(ratio, i)
             output_fname = TO_DATA + "{}/{}".format(ratio, i)
-            commands = ["python",
-                        TO_CITRUSS,
+            commands = [TO_CITRUSS,
                         str(N), str(Q), str(P),
                         ysum_fname,
                         ym_fname,
                         yp_fname,
                         xm_fname,
                         xp_fname,
-                        output_fname,
-                        str(reg_v), str(reg_f), str(reg_gamma), str(reg_psi)]
+                        str(reg_v), str(reg_f), str(reg_gamma), str(reg_psi),
+                        '-o', output_fname]
             subprocess.run(commands, check=True)
 
 
 if __name__ == '__main__':
+    start_main = time.time()
     main()
+    print("Program ran in {:.3f} seconds".format(time.time() - start_main), 
+          file=sys.stderr)
